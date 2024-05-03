a0f75caeef931cb2008a95cfa1affd7314f986fa
tommasobrivio <tommasobrivio150@gmail.com>
2024-05-03 18:44:39 +0200

    Adding database. Update E/R model.

---
+++ sito/database/stazioni_biciclette.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mag 03, 2024 alle 18:40
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stazioni_biciclette`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `admin`
+--
+
+CREATE TABLE `admin` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `passsword` varchar(32) NOT NULL,
+  `nome` varchar(32) NOT NULL,
+  `cognome` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `bicicletta`
+--
+
+CREATE TABLE `bicicletta` (
+  `ID` int(11) NOT NULL,
+  `distanza_percorsa` float DEFAULT NULL,
+  `latitudine` varchar(10) NOT NULL,
+  `longitudine` varchar(10) NOT NULL,
+  `gps` varchar(16) NOT NULL,
+  `stato` tinyint(4) NOT NULL,
+  `RFID` varchar(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `clienti`
+--
+
+CREATE TABLE `clienti` (
+  `ID` int(11) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nome` varchar(32) DEFAULT NULL,
+  `cognome` varchar(32) DEFAULT NULL,
+  `numero_tessera` int(11) DEFAULT NULL,
+  `carta_credito` int(11) NOT NULL,
+  `regione` varchar(32) DEFAULT NULL,
+  `provincia` varchar(32) DEFAULT NULL,
+  `citta` varchar(32) DEFAULT NULL,
+  `cap` int(5) DEFAULT NULL,
+  `via` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `operazione`
+--
+
+CREATE TABLE `operazione` (
+  `ID` int(11) NOT NULL,
+  `distanza_percorsa` float NOT NULL,
+  `tipo` enum('noleggio','riconsegna') NOT NULL,
+  `tariffa` float DEFAULT NULL,
+  `data_ora` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `id_cliente` int(11) NOT NULL,
+  `id_bicicletta` int(11) NOT NULL,
+  `id_stazione` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `stazione`
+--
+
+CREATE TABLE `stazione` (
+  `ID` int(11) NOT NULL,
+  `codice` int(11) NOT NULL,
+  `slot` int(11) NOT NULL,
+  `regione` varchar(32) NOT NULL,
+  `provincia` varchar(32) NOT NULL,
+  `citta` varchar(32) NOT NULL,
+  `cap` int(5) NOT NULL,
+  `via` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `bicicletta`
+--
+ALTER TABLE `bicicletta`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indici per le tabelle `clienti`
+--
+ALTER TABLE `clienti`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `numero_tessera` (`numero_tessera`);
+
+--
+-- Indici per le tabelle `operazione`
+--
+ALTER TABLE `operazione`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `id_cliente` (`id_cliente`,`id_bicicletta`,`id_stazione`);
+
+--
+-- Indici per le tabelle `stazione`
+--
+ALTER TABLE `stazione`
+  ADD PRIMARY KEY (`ID`,`codice`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `bicicletta`
+--
+ALTER TABLE `bicicletta`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `clienti`
+--
+ALTER TABLE `clienti`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `operazione`
+--
+ALTER TABLE `operazione`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `stazione`
+--
+ALTER TABLE `stazione`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
