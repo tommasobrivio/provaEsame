90c7ca7dfb23127a0b8ec60e982bbc7ff449cbaa
tommasobrivio <tommasobrivio150@gmail.com>
2024-05-23 18:51:59 +0200

    Update database

--- sito/database/stazioni_biciclette.sql
+++ sito/database/stazioni_biciclette.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 22, 2024 alle 09:25
+-- Creato il: Mag 23, 2024 alle 18:30
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -55,12 +55,12 @@ INSERT INTO `admin` (`ID`, `email`, `username`, `password`, `nome`, `cognome`) V
 
 CREATE TABLE `bicicletta` (
   `ID` int(11) NOT NULL,
-  `distanza_percorsa` float DEFAULT NULL,
+  `distanza_percorsa` float DEFAULT 0,
   `latitudine` varchar(10) NOT NULL,
   `longitudine` varchar(10) NOT NULL,
-  `gps` varchar(16) NOT NULL,
-  `stato` tinyint(4) NOT NULL,
-  `RFID` varchar(16) NOT NULL
+  `gps` varchar(16) DEFAULT NULL,
+  `stato` enum('disponibile','non disponibile','in manutenzione') NOT NULL,
+  `RFID` varchar(16) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -68,11 +68,25 @@ CREATE TABLE `bicicletta` (
 --
 
 INSERT INTO `bicicletta` (`ID`, `distanza_percorsa`, `latitudine`, `longitudine`, `gps`, `stato`, `RFID`) VALUES
-(1, 100.5, '45.6789', '9.1234', 'GPS123456789', 1, 'RFID123456789'),
-(2, 75.25, '45.6781', '9.1236', 'GPS987654321', 0, 'RFID987654321'),
-(3, 150.75, '45.6790', '9.1240', 'GPS246813579', 1, 'RFID246813579'),
-(4, 50, '45.6775', '9.1220', 'GPS369258147', 1, 'RFID369258147'),
-(5, 80.3, '45.6770', '9.1215', 'GPS135792468', 0, 'RFID135792468');
+(7, 0, '45.7402572', '9.122636', '1001', 'non disponibile', '1001'),
+(8, 0, '45.7402572', '9.122636', '1002', 'disponibile', '1002'),
+(9, 0, '45.7325526', '9.1339923', '1003', 'disponibile', '1003');
+
+--
+-- Trigger `bicicletta`
+--
+DELIMITER $$
+CREATE TRIGGER `RFID` BEFORE INSERT ON `bicicletta` FOR EACH ROW BEGIN
+SET NEW.RFID = IFNULL((SELECT MAX(RFID) + 1 FROM bicicletta), 1001);
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `gps` BEFORE INSERT ON `bicicletta` FOR EACH ROW BEGIN
+SET NEW.gps = IFNULL((SELECT MAX(gps) + 1 FROM bicicletta), 1001);
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -101,11 +115,11 @@ CREATE TABLE `clienti` (
 --
 
 INSERT INTO `clienti` (`ID`, `email`, `username`, `password`, `nome`, `cognome`, `numeroTessera`, `carta_credito`, `regione`, `provincia`, `citta`, `cap`, `via`) VALUES
-(1, 'cliente1@example.com', 'mario_', 'de2f15d014d40b93578d255e6221fd60', 'Mario', 'Rossi', 1001, '2147483647', 'Lombardia', 'Como', 'Cernobbio', 22012, 'Via Roma 1'),
-(2, 'cliente2@example.com', 'cliente2', '73a054cc528f91ca1bbdda3589b6a22d', 'Luigi', 'Verdi', 1002, '2147483646', 'Lombardia', 'Como', 'Como', 22100, 'Via Garibaldi 10'),
-(3, 'cliente3@example.com', 'cliente3', 'ba1b5d9d26dd50164b5fb53a948e5cdf', 'Giovanna', 'Bianchi', 1003, '2147483641', 'Lombardia', 'Como', 'Como', 22100, 'Via Alessandro Volta 5'),
-(4, 'cliente4@example.com', 'cliente4', 'e38561d99e538eb7d936acb92bd847b0', 'Anna', 'Neri', 1004, '2147483642', 'Lombardia', 'Como', 'Como', 22100, 'Via Milano 20'),
-(5, 'cliente5@example.com', 'cliente5', '1dac2d5e70b56800ba9cdf6e877d519b', 'Paolo', 'Gialli', 1005, '2147483643', 'Lombardia', 'Como', 'Como', 22100, 'Via Como 15'),
+(1, 'cliente1@example.com', 'mario_', 'de2f15d014d40b93578d255e6221fd60', 'Marioo', 'Rossi', 1001, '2147483647000125', 'Lombardia', 'Como', 'Cernobbio', 22012, 'Via Roma 1'),
+(2, 'cliente2@example.com', 'cliente2', '73a054cc528f91ca1bbdda3589b6a22d', 'Luigi', 'Verdi', 1002, '2147483646000132', 'Lombardia', 'Como', 'Como', 22100, 'Via Garibaldi 10'),
+(3, 'cliente3@example.com', 'cliente3', 'ba1b5d9d26dd50164b5fb53a948e5cdf', 'Giovanna', 'Bianchi', 1003, '2147483641000213', 'Lombardia', 'Como', 'Como', 22100, 'Via Alessandro Volta 5'),
+(4, 'cliente4@example.com', 'cliente4', 'e38561d99e538eb7d936acb92bd847b0', 'Anna', 'Neri', 1004, '2147483642000231', 'Lombardia', 'Como', 'Como', 22100, 'Via Milano 20'),
+(5, 'cliente5@example.com', 'cliente5', '1dac2d5e70b56800ba9cdf6e877d519b', 'Paolo', 'Gialli', 1005, '2147483643000321', 'Lombardia', 'Como', 'Como', 22100, 'Via Como 15'),
 (6, 'luigi@', 'luigi_', 'e9da82f4c252e7f1745ae88f2624fc07', 'luigi', 'verdi', 1006, '0000000000000000', 'Lombardia', 'Como', 'Cantù', 22063, 'via verona 8');
 
 --
@@ -126,7 +140,7 @@ DELIMITER ;
 
 CREATE TABLE `operazione` (
   `ID` int(11) NOT NULL,
-  `distanza_percorsa` float NOT NULL,
+  `distanza_percorsa` float DEFAULT NULL,
   `tipo` enum('noleggio','riconsegna') NOT NULL,
   `tariffa` float DEFAULT NULL,
   `data_ora` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
@@ -140,11 +154,7 @@ CREATE TABLE `operazione` (
 --
 
 INSERT INTO `operazione` (`ID`, `distanza_percorsa`, `tipo`, `tariffa`, `data_ora`, `id_cliente`, `id_bicicletta`, `id_stazione`) VALUES
-(1, 10.75, 'noleggio', 5.5, '2024-05-10 06:20:33', 1, 1, 1),
-(2, 8.2, 'noleggio', 4, '2024-05-10 06:20:33', 2, 2, 2),
-(3, 15.5, 'riconsegna', 0, '2024-05-10 06:20:33', 3, 3, 3),
-(4, 5.3, 'noleggio', 3.25, '2024-05-10 06:20:33', 4, 4, 4),
-(5, 20.1, 'riconsegna', 0, '2024-05-10 06:20:33', 5, 5, 5);
+(6, NULL, 'noleggio', NULL, '2024-05-23 16:29:54', 1, 9, 1002);
 
 -- --------------------------------------------------------
 
@@ -159,8 +169,28 @@ CREATE TABLE `stazione` (
   `provincia` varchar(32) NOT NULL,
   `citta` varchar(32) NOT NULL,
   `cap` int(5) NOT NULL,
-  `via` varchar(64) NOT NULL
+  `via` varchar(64) NOT NULL,
+  `latitudine` varchar(32) DEFAULT NULL,
+  `longitudine` varchar(32) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `stazione`
+--
+
+INSERT INTO `stazione` (`codice`, `slot`, `regione`, `provincia`, `citta`, `cap`, `via`, `latitudine`, `longitudine`) VALUES
+(1002, 22, 'lombardia', 'como', 'cantù', 22063, 'via Alessandro Manzoni', '45.7402572', '9.122636'),
+(1003, 2, 'Lombardia', 'Como', 'Cantù', 22063, 'via Verona 8', '45.7325526', '9.1339923');
+
+--
+-- Trigger `stazione`
+--
+DELIMITER $$
+CREATE TRIGGER `test2` BEFORE INSERT ON `stazione` FOR EACH ROW BEGIN
+SET NEW.codice = IFNULL((SELECT MAX(codice) + 1 FROM stazione), 1001);
+END
+$$
+DELIMITER ;
 
 --
 -- Indici per le tabelle scaricate
@@ -215,25 +245,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT per la tabella `bicicletta`
 --
 ALTER TABLE `bicicletta`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT per la tabella `clienti`
 --
 ALTER TABLE `clienti`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT per la tabella `operazione`
 --
 ALTER TABLE `operazione`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT per la tabella `stazione`
---
-ALTER TABLE `stazione`
-  MODIFY `codice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Limiti per le tabelle scaricate
@@ -244,13 +268,8 @@ ALTER TABLE `stazione`
 --
 ALTER TABLE `operazione`
   ADD CONSTRAINT `operazione_bicicletta` FOREIGN KEY (`id_bicicletta`) REFERENCES `bicicletta` (`ID`),
-  ADD CONSTRAINT `operazione_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clienti` (`ID`);
-
---
--- Limiti per la tabella `stazione`
---
-ALTER TABLE `stazione`
-  ADD CONSTRAINT `stazione_ibfk_1` FOREIGN KEY (`codice`) REFERENCES `operazione` (`id_stazione`);
+  ADD CONSTRAINT `operazione_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `clienti` (`ID`),
+  ADD CONSTRAINT `operazione_stazione` FOREIGN KEY (`id_stazione`) REFERENCES `stazione` (`codice`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
